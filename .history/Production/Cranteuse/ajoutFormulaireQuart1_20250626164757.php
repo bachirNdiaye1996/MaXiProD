64c1242438133cebab7f14ca5bb8a6d854382b10
Bachir <mouhamadoulbachir2@gmail.com>
2025-06-26 16:50:22 +0000

    erreur.

--- .history/Production/Cranteuse/ajoutFormulaireQuart1_20250626164757.php
+++ .history/Production/Cranteuse/ajoutFormulaireQuart1_20250626164757.php
@@ -669,6 +669,76 @@
                                                         </div>
                                                     </div>
 
+                                                    <div class="col-lg-5 mt-2">
+                                                        <h5 class="modal-title mb-3 text-center font-weight-bold text-primary" id="myExtraLargeModalLabel">Gestion des temps d'arret</h5>
+                                                        <table class="table table-bordered" id="" width="100%" cellspacing="0">
+                                                            <thead>
+                                                                <tr>       
+                                                                    <th>Début arrets</th>
+                                                                    <th>Fin arrets</th>
+                                                                    <th>Raisons</th>
+                                                                    <th>Supprimer ligne</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody id="dynamicaddErreurs">
+                                                                <?php
+                                                                    //$i=0;
+                                                                    //for ($i = 0; $i <= $NombreLigne; $i++){
+                                                                        //$i++;
+                                                                        //if($article['status'] == 'termine'){
+                                                                ?>
+                                                                    <tr class="rowClass">
+                                                                        <td style="background-color:#CFFEDA;">
+                                                                            <div class="col-md-10">
+                                                                                <div class="mb-1 text-start">
+                                                                                    <input class="form-control" id="validationDefault04" type="time" name="debutarret[]" required>
+                                                                                </div>
+                                                                            </div>
+                                                                        </td>
+                                                                        <td style="background-color:#CFFEDA;">
+                                                                            <div class="col-md-10">
+                                                                                <div class="mb-1 text-start">
+                                                                                    <input class="form-control designa" type="time" name="finarret[]" value="" required>
+                                                                                </div>
+                                                                            </div>
+                                                                        </td>
+                                                                        <!--<td style="background-color:#CFFEDA;">
+                                                                            <div class="col-md-10">
+                                                                                <div class="mb-1 text-start">
+                                                                                    <select class="form-control" name="raisonerreur[]">
+                                                                                        <option>Raison 1</option>
+                                                                                        <option>Raison 2</option>
+                                                                                        <option>Raison 3</option> 
+                                                                                    </select>                                                
+                                                                                </div>
+                                                                            </div>
+                                                                        </td>!-->
+                                                                        <td style="background-color:#CFFEDA;">
+                                                                            <div class="col-md-10">
+                                                                                <div class="mb-1 text-start">
+                                                                                    <input class="form-control" id="validationDefault01" type="text" name="raisonerreur[]" value="" placeholder="Mettez la raison d'erreur" required>
+                                                                                </div>
+                                                                            </div>
+                                                                        </td>
+                                                                        <td style="background-color:#CFFEDA;" class="text-center"> 
+                                                                            <button class="btn btn-danger removeErreurs"
+                                                                                type="button">Enlever
+                                                                            </button> 
+                                                                        </td>
+                                                                    </tr>
+                                                                <?php
+                                                                // }
+                                                                ?>
+                                                            </tbody>
+                                                        </table>
+
+                                                        <div class="col-md-7  d-flex gap-2">
+                                                            <div class="mb-5 text-start d-flex gap-2 pt-4">
+                                                                <input class="btn btn-success  w-lg bouton mr-3" name="ChangerNombreLigne" id="addErreurs" type="button" value="Ajouter une ligne">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
                                                     <div class="col-lg-7 mt-2">
                                                         <h5 class="modal-title mb-3 text-center font-weight-bold text-primary" id="myExtraLargeModalLabel">Consommations</h5>
                                                         <table class="table table-bordered" id="" width="100%" cellspacing="0">
@@ -683,7 +753,7 @@
                                                                     <th>Supprimer ligne</th>
                                                                 </tr>
                                                             </thead>
-                                                            <tbody id="dynamicaddConsommations">
+                                                            <tbody class="dynamicaddConsommations">
                                                                 <?php
                                                                     $i=0;
                                                                     //for ($i = 0; $i <= $NombreLigne; $i++){
@@ -774,7 +844,6 @@
                                                         </div>
                                                     </div>
 
-
                                                     <div class="col-lg-12">
                                                         <h5 class="modal-title mb-3 text-center font-weight-bold text-primary" id="myExtraLargeModalLabel">Productions</h5>
                                                         <table class="table table-bordered" id="" cellspacing="0">
@@ -790,7 +859,7 @@
                                                                     <th>Supprimer ligne</th>
                                                                 </tr>
                                                             </thead>
-                                                            <tbody id="dynamicaddProductions">
+                                                            <tbody class="dynamicaddConsommations">
                                                                 <?php
                                                                     //$i=0;
                                                                     //for ($i = 0; $i <= $NombreLigne; $i++){
